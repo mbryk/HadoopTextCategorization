4e3bf25af3f9985d3a2377cc19ac1487fada9f14
Mark Bryk <brykcu@gmail.com>
2014-05-11 15:08:44 -0400

    setting up map and reduce for knn

--- src/main/java/ece465/ReduceClassKNN.java
+++ src/main/java/ece465/ReduceClassKNN.java
@@ -1,6 +1,29 @@
 package ece465;
+/*
+The Reducer gets key=testCaseID, value=iterable array of [class,similarity].
+It loops through this array, and finds the class with the highest similarity.
+It returns key=testCaseID,value=class
+*/
 
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class ReduceClassKNN extends Reducer {
+public static class ReduceClassKNN extends Reducer<IntWritable, Text, LongWritable, Text> {
+        public void reduce(IntWritable key, Iterable<MapOutputKNN> values, Context context) throws IOException, InterruptedException {
+
+            IntWritable tclass = new IntWritable();
+           
+			int max = null;
+			int tmpclass = null;
+ 
+            for (MapOutputKNN val : values) {
+				if(max ==null || val.sim>max){
+					max = val.sim;
+					tmpclass = val.tclass;
+				}
+			}
+
+			tclass.set(tmpclass);
+            context.write(key, tclass);
+            tclass.clear();
+        }
 }
